cc7698ce5826650b0fb2b1a35620a9d57ca25bb5
Stephen Becker IV <github@deathbyescalator.com>
2016-01-17 09:47:42 -0800

    Copied from chapter-2
    Code used for parsing incoming files. Need to make general

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,32 @@
 pub mod timer;
 pub mod stdin_lines;
+use std::path::Path;
+use std::io::prelude::*;
+use std::fs::File;
+
+pub fn read_file_lines(file_string: String) -> Vec<String> {
+    let file = Path::new(&file_string);
+    let mut open_file = File::open(file).unwrap();
+    let mut buffer = String::new();
+    open_file.read_to_string(&mut buffer).unwrap();
+    buffer.lines().map(|num| num.to_string()).collect::<Vec<String>>()
+}
+
+pub fn read_file_chars_as_numbers(file_string: String) -> Vec<isize> {
+    let file = Path::new(&file_string);
+    let mut open_file = File::open(file).unwrap();
+    let mut buffer = String::new();
+    open_file.read_to_string(&mut buffer).unwrap();
+    buffer.split_whitespace().map(|num| num.parse::<isize>().unwrap()).collect::<Vec<isize>>()
+}
+
+pub fn read_file_chars(file_string: String) -> Vec<String> {
+    let file = Path::new(&file_string);
+    let mut open_file = File::open(file).unwrap();
+    let mut buffer = String::new();
+    open_file.read_to_string(&mut buffer).unwrap();
+    buffer.split_whitespace().map(|num| num.to_string()).collect::<Vec<String>>()
+}
 
 #[test]
 fn it_works() {}
